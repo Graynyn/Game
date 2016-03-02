c1d7f3e70d35dfd63bdd3aa3f5c311014f8c7668
Graynyn <nicolas.yax@sfr.fr>
2016-03-02 12:30:33 +0100

    Update Terrain.py

--- Terrain.py
+++ Terrain.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 class Terrain:
     """ Classe regroupant les informations relatives au Terrain de jeu """
     #Attributs GAME
@@ -17,9 +19,9 @@ class Terrain:
             for j in range (height):
                 k=i%2
                 if k ==1:
-                    self.label(i,j,bg="AAAAAA")
+                    self._labels(i,j,bg="AAAAAA") #Corrections de nico : Attention : C'est self._labels et pas self.label !
                 if k ==0:
-                    self.label(i,j,bg="000000")
+                    self.label(i,j,bg="000000") #Je te laisse corriger ici
 
     def __init__(self,width,height,tabl_points,root):
         """ initialisateur de la classe terrain"""
@@ -34,7 +36,7 @@ class Terrain:
         #Racine
         self._root = root
         #Attributs Graphics
-        self._can = Canvas(self._root._fen)
+        self._can = tk.Canvas(self._root._fen)
         self._can.grid(column=1,row=1)
         
     def add_card(self,x,y,card):
@@ -43,7 +45,13 @@ class Terrain:
         # On met a jour les coords en attr des cartes
         card._x = x
         card._y = y
-                
+        
+    def remove_card(self,x,y):
+        """ On retire une carte du tableau """
+        self._tabl_cards[x][y]._x = None #On met la valeur de l'attr _x de la carte à None ce qui veut dire qu'elle n'est plus sur le terrain
+        self._tabl_cards[x][y]._y = None
+        self._tabl_cards[x][y] = None
+        
     def reset_action(self):
         """ On permet aux cartes d'attaquer et à se déplacer """
         #On parcours tout le terrain (x et y)
